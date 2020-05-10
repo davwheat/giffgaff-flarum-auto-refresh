483f9b8f092f610a96fc7870f0e6343b6ece4239
David Wheatley <davidwheatley03@gmail.com>
2020-05-10 13:28:55 +0100

    Refactor editable items into variables

--- flarum-auto-refresh.user.js
+++ flarum-auto-refresh.user.js
@@ -3,23 +3,33 @@
 // @description    Automatically refreshes the H&S discussion list every 20 seconds
 // @author         David Wheatley <davidwheatley03@gmail.com> (https://github.com/davwheat/giffgaff-flarum-auto-refresh)
 // @namespace      https://github.com/davwheat/giffgaff-flarum-auto-refresh
-// @version        1.1.0
+// @version        1.2.0
 // @icon           https://github.com/davwheat/giffgaff-flarum-auto-refresh/blob/master/icon.png?raw=true
 // @match          *://community.giffgaff.com/*
 // @grant          none
 // @run-at         document-end
 // ==/UserScript==
 
+// Interval between refreshes (seconds)
+const refreshInterval = 20;
+
+// list of enabled paths
+// !! don't include any query parameters (e.g. ?sort=xxxx) !!
+const enabledPaths = [
+    "/t/help-and-support"
+];
+
 setInterval(() => {
-    if (window.location.pathname !== "/t/help-and-support") {
-        console.info("Not refreshing discussion list: not on H&S tag page.")
+    if (enabledPaths.includes(window.location.pathname)) {
+        console.info("Not refreshing discussion list. Page not on the enables paths list.", enabledPaths)
         return
     }
     
+    // literally just find the refresh button and click it lol
     $("main.App-content  div.IndexPage-toolbar li.item-refresh > button").click()
     
     console.info("Refreshed discussion list.");
-}, 20 * 1000);
+}, refreshInterval * 1000);
 
 
 console.log(`%c Don't paste anything down here which someone tells you to!`, 'color: red; font-size: 64px; font-weight: bold; background: yellow;');
